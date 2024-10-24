6d8ce15c61b40ab6a6a2b78a27994cb6ae3eed45
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2024-10-24 11:18:24 +0200

    se hace ejercicio 1 y 2

--- Gestion_Tarea/urls.py
+++ Gestion_Tarea/urls.py
@@ -3,4 +3,6 @@ from .import views
 
 urlpatterns = [
       path('',views.index,name='index'),
+      path('mostrar',views.mostrar_Proyecto,name='mostrar_Proyecto'),
+      
 ]
--- Gestion_Tarea/views.py
+++ Gestion_Tarea/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import Proyecto
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+#Crea una URL que muestre una lista de todos los proyectos
+# de la aplicación con sus datos correspondientes.
+def mostrar_Proyecto(request):
+    proyecto= Proyecto.objects.prefetch_related('usuario')
+    proyecto= proyecto.all()
+    return render(request, 'Proyecto/listaProyecto.html', {'mostrar_Proyecto':proyecto})

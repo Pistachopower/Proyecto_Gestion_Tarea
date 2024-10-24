45aa7209380ecf165a0f20adf122983b1ae37444
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2024-10-24 22:41:32 +0200

    se avanza algo pero faltan detalles

--- Gestion_Tarea/urls.py
+++ Gestion_Tarea/urls.py
@@ -4,5 +4,7 @@ from .import views
 urlpatterns = [
       path('',views.index,name='index'),
       path('mostrar',views.mostrar_Proyecto,name='mostrar_Proyecto'),
+      path('tarea',views.tareas_Proyecto,name='tareas_Proyecto'),
+      path('usuariotarea',views.usuario_Tarea,name='usuario_Tarea'),
       
 ]
--- Gestion_Tarea/views.py
+++ Gestion_Tarea/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Proyecto
+from .models import Proyecto, Tarea, Proyecto, Usuario
 
 # Create your views here.
 def index(request):
@@ -9,6 +9,23 @@ def index(request):
 #Crea una URL que muestre una lista de todos los proyectos
 # de la aplicación con sus datos correspondientes.
 def mostrar_Proyecto(request):
-    proyecto= Proyecto.objects.prefetch_related('usuario')
-    proyecto= proyecto.all()
+    proyecto= Proyecto.objects.prefetch_related('usuario').all()
+    #proyecto= proyecto.all()
     return render(request, 'Proyecto/listaProyecto.html', {'mostrar_Proyecto':proyecto})
+
+"""
+Crear una URL que muestre todas las tareas que están asociadas a un proyecto, 
+ordenadas por fecha de creación descendente.
+"""
+def tareas_Proyecto(request):
+    tareasAsoPro= Tarea.objects.select_related('proyecto').all()
+    return render(request, 'TareaProyecto/tareaProyecto.html', {'tareas_Proyecto':tareasAsoPro})
+
+
+"""
+Crear una URL que muestre todos los usuarios que están asignados a una tarea 
+ordenados por la fecha de asignación de la tarea de forma ascendente. 
+"""
+def usuario_Tarea(request):
+    usuarioTarea= Usuario.objects.prefetch_related("tarea").all()
+    return render(request, 'UsuarioTarea/UsuarioTarea.html', {'usuario_Tarea':usuarioTarea})

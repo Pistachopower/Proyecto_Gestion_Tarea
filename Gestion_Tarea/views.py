fea56c2eb3648223694299f0bc236067df5f27f3
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2024-10-25 14:46:08 +0200

    se sube ejercdicio 5

--- Gestion_Tarea/views.py
+++ Gestion_Tarea/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Proyecto, Tarea, Proyecto, Usuario
+from django.db.models import Q
+from .models import Proyecto, Tarea, Proyecto, Usuario, Asignacion_Tarea
 
 # Create your views here.
 def index(request):
@@ -14,18 +15,21 @@ def mostrar_Proyecto(request):
     return render(request, 'Proyecto/listaProyecto.html', {'mostrar_Proyecto':proyecto})
 
 """
-Crear una URL que muestre todas las tareas que están asociadas a un proyecto, 
-ordenadas por fecha de creación descendente.
+Crear una URL que muestre todas las tareas que están asociadas a un
+proyecto, ordenadas por fecha de creación descendente.
 """
 def tareas_Proyecto(request):
-    tareasAsoPro= Tarea.objects.select_related('proyecto').all()
+    tareasAsoPro= Tarea.objects.select_related('proyecto')
+    tareasAsoPro= tareasAsoPro.order_by('-Fecha_Creación')
     return render(request, 'TareaProyecto/tareaProyecto.html', {'tareas_Proyecto':tareasAsoPro})
+
 
 
 """
 Crear una URL que muestre todos los usuarios que están asignados a una tarea 
 ordenados por la fecha de asignación de la tarea de forma ascendente. 
 """
-def usuario_Tarea(request):
-    usuarioTarea= Usuario.objects.prefetch_related("tarea").all()
+def usuario_Tarea(request,tarea_id):
+    tarea= Tarea.objects.get(id= tarea_id)
+    usuarioTarea= Asignacion_Tarea.objects.filter(tarea= tarea).select_related('usuario').order_by('fecha_asignacion')
     return render(request, 'UsuarioTarea/UsuarioTarea.html', {'usuario_Tarea':usuarioTarea})
